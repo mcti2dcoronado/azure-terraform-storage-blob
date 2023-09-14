d7fa65f07ecb03dd26f5d089693d824e3644c2aa
Douglass Coronado <dcoronado@funio.com>
2023-09-13 20:57:37 -0400

    Add resources

--- main.tf
+++ main.tf
@@ -3,3 +3,25 @@ resource "azurerm_resource_group" "azureresourcegroup" {
   name     = "MCIT_resource_group"
   location = "Canada Central"
 }
+
+resource "azurerm_storage_account" "my_storage_account" {
+  name                     = "my-storage-account"
+  resource_group_name      = azurerm_resource_group.azureresourcegroup.name
+  location                 = azurerm_resource_group.azureresourcegroup.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+}
+
+resource "azurerm_storage_container" "my_container" {
+  name                  = "my_container"
+  storage_account_name  = azurerm_storage_account.my_storage_account.name
+  container_access_type = var.container_access_type
+}
+
+resource "azurerm_storage_blob" "my_storage_blob" {
+  name                   = "my-zip"
+  storage_account_name   = azurerm_storage_account.my_stroage_account.name
+  storage_container_name = azurerm_storage_container.my_container.name
+  type                   = var.blob_type
+  source                 = "var.blob_source"
+}
--- variable.tf
+++ variable.tf
@@ -10,3 +10,28 @@ variable "subscription_id"{
 variable "tenant_id"{
   type=string
 }
+
+variable "account_tier"{
+  type = string
+  default = "Standard"
+}
+
+variable "account_replication_type"{
+  type = string
+  default = "LRS"
+}
+
+variable "container_access_type"{
+  type = string
+  default = "private"
+}
+
+variable "blob_type"{
+  type = string
+  default = "Block"
+}
+  
+variable "blob_source"{
+  type = string
+  default = "example.zip"
+}
